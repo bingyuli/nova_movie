10c2cb17119ef56b442fe8a44822da4643f737f1
shibai <shibai86@gmail.com>
2013-11-19 01:02:15 -0800

    add fetchMovie and movie files

---
+++ public/fetchMovie.php
@@ -0,0 +1,12 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/db_connection.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php require_once("../includes/validation_functions.php"); ?>
+<?
+  //$id = $_POST['movieID'];
+  //$query = "select name,year,director,rating,indroduction from movie where id = '{$id}'";  
+  //$result = mysqli_query($connection, $query);	
+
+  //echo json_encode("aa");
+  echo("ccc");
+?>
