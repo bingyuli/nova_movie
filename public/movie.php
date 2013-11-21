5767fb923dfab20f00543d9287e0f5aaacd0d8af
shibai <shibai86@gmail.com>
2013-11-20 21:33:38 -0800

    select by id, add session checking

--- public/movie.php
+++ public/movie.php
@@ -1,20 +1,46 @@
-
+<?php
+  session_start();
+  
+  // check for user log-in
+  if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    //echo $_SESSION['userID'];
+  } else {
+	$url = 'http://localhost/public/user_login.php';	
+    //header("Location: $url");
+  }
+  //echo $_SESSION['userID'];
+?>
 
 <script src="http://code.jquery.com/jquery-latest.js"></script>
 <script>
 
-  function shpwMovie () {
-    //alert("aaaaaa");
-	var d;
-	$.get( "fetchMovie.php", { name: "John" }, function( data ) {
-	  d = data;
+  // show all movies
+  function showAllMovie () {
+	$.get( "fetchMovie.php", { name: "John" }, function( d ) {
 	  var html = '';
 	  for(var i = 0; i < d.length; i++)
-            html += '<tr><td>' + d[i].name + '</td><td>' + d[i].year + '</td><td>' 
-			+ d[i].director + '</td><td>' + d[i].rating + '</td><td>' + d[i].introduction + "</td></tr>";
+            html += "<tr><td id="+ d[i].id +" onClick='addToIn(this)'><font color='red'>Save</font></td><td>" 
+			+ d[i].name + '</td><td>' + d[i].year + '</td><td>' + d[i].director + '</td><td>' + d[i].rating + '</td><td>' 
+			+ d[i].introduction + "</td><td id="+ d[i].id +" onClick='addToIn(this)'>Watch this</td></tr>";
 	$('#movieTable tr').first().after(html);
     },"json");
 
+  }
+  
+  // add to interested or watched table
+  function addToIn() {
+    //..alert(movie.id);
+	$.get( "fetchMovie.php", { movieId: 1,aol: 2 }, function( data ) {
+	  alert(data)
+	});
+  }
+  
+  // show current user's interested or watched movie list
+  function showInterested(movie) {
+    //..alert(movie.id);
+	$.get( "fetchMovie.php", { movieId: movie.id }, function( data ) {
+	  alert(movie.id)
+	});
   }
   
 </script>
@@ -25,22 +51,22 @@
 </div>
 <div>
   <p>movie ID <br></br>
-       <input type="text" id="movieID" value="" />
-	   <input type="button" onclick="shpwMovie()" value="show movie"/>
+	   <input type="button" onclick="showAllMovie()" value="show all movies"/>
+	   <input type="button" onclick="addToIn()" value="show interested movies"/>
   </p>
-  <div id = "1ab"></div>
   
 </div>
 <body>
   <table border="1" id = "movieTable">
 	<tr>
+	<td>SaveToInterest</td>
 	<th>Title</th>
 	<th>Year</th>
 	<th>Director</th>
 	<th>Rating</th>
 	<th>Introduction</th>
+	<td></td>
 	</tr>
   </table> 
-
 
 </body>
